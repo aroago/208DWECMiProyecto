5a4ca0023a7fcdd03fdedc1e5a68449b05e7a6db
aroa granero <aroa.graoma@educa.jcyl.es>
2022-02-18 13:53:22 +0100

    18/02/22 actualizacion de ejercicios

---
+++ indexProyectoDWEC.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--Aroa Granero Omañas 
+Fecha Creacion: 30/11/2021
+Fecha Modificacion: 30/09/2021 -->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="aroaGraneroOmañas">
+        <meta name="application-name" content="Sitio web de DAW2">
+        <meta name="description" content="Inicio de la pagina web">
+        <meta name="keywords" content=" index" >
+        <meta name="robots" content="index, follow" />
+        <link href="./webroot/css/estilos.css"  rel="stylesheet"  type="text/css" title="Default style">
+        <link rel="shortcut icon" href="favicon.ico">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Aroa</title>
+    </head>
+    <body>
+        <header>
+            <h1>AROA G.O desde 2001</h1>
+        </header>
+        <main>
+            <div id="divLista">
+                <ol id="lista">
+                    <li>WEB DWEC by Aroa</li>
+					<li><a href="./PR-1-7-1.html">Ejercicio Ajax</li>
+                </ol>
+				
+            </div>
+        </main>
+        <footer id="footerP">
+            <p>&copy;2021 Todos los derechos reservados AroaGO</p>
+            <div id="iconos">
+                <a type="application/github" href="https://github.com/aroago" target="_blank">
+                    <img class="iconoIMG" alt="gitHub" src="./webroot/img/github.png" />
+                </a>
+            </div>
+        </footer>
+    </body>
+</html>
